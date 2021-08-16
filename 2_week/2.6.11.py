1e911d8c278a19c553877e287cc8a9033ad5ca76
unlaik <unlaik24@gmail.com>
2021-08-17 00:23:51 +0300

    Added 2.6.11 dinamical matrix

--- 2_week/2.6.11.py
+++ 2_week/2.6.11.py
@@ -1,18 +1,28 @@
 InputNumber=int(input())
-x=1
-y=InputNumber
-Matrix=[[0 for i in range(InputNumber)] for j in range(InputNumber)]
-for i in range(1):
-    for j in range(InputNumber):
-        Matrix[i][j] +=x
-        x+=1
-while 0 not in Matrix:
-    for j in range(1):
-        for i in range(1):
-            Matrix[i][j] +=x
-            x+=1
+Matrix = [[0] * InputNumber for i in range(InputNumber)] #генерация матрицы n*n  и заполнение её нулями
+Matrix[0] = [(col+1) for col in range(InputNumber)] #проход по самой верхней грани матрицы через генератор
+y = 0; x = InputNumber-1; row=InputNumber; index = InputNumber # так как проход по верхней грани уже сделан,индекс равен верхней грани, y - столбец, х - строка
+while index < InputNumber*InputNumber: #пока индекс не больше n*n
+    row -=1 #уменьшения длины на 1
+    for column in range(row):# вниз
+        y += 1 #направление вниз
+        index += 1
+        Matrix[y][x] = index
+    for column in range(row):     #влево
+        x -= 1 #направление влево
+        index += 1
+        Matrix[y][x] = index
+    if index >= InputNumber * InputNumber:
+        break
+    row -=1 #уменьшения длины на 1
+    for column in range(row): # вверх
+        y -= 1 #направление вверх
+        index += 1
+        Matrix[y][x] = index
+    for column in range(row): # вправо
+        x += 1 #направление вправо
+        index += 1
+        Matrix[y][x] = index
 
-       # for j in range(y,0,-1):
-        #    for i in range(y,-1,-1):
-         #       Matrix[i][j] += x
-          #      x += 1
+for line in Matrix:
+    print("\t".join(map(str, line)))
